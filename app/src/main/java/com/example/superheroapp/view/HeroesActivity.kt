a72eca744d1b04c38024af9af0b832ae0cc6d77e
RamyaD00 <ramyadeivasigamani22@gmail.com>
2021-04-08 17:56:37 +0530

    app

--- app/src/main/java/com/example/superheroapp/view/HeroesActivity.kt
+++ app/src/main/java/com/example/superheroapp/view/HeroesActivity.kt
@@ -20,6 +20,7 @@ class HeroesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
 
         val navController = findNavController(R.id.nav_host_fragment)
